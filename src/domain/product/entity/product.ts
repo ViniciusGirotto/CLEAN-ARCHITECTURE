50f46a4ae14304322ee54a4267576a4deff4cd10
ViniciusGirotto <vinicius.s.carvalho23@gmail.com>
2024-09-24 23:50:16 -0300

    Add main.ts, product.ts, package.json, and .gitignore files

---
+++ src/domain/product/entity/product.ts
@@ -0,0 +1,12 @@
+export type ProductProps = {
+  id: string;
+  name: string;
+  price: number;
+  stock: number;
+  description: string;
+  image: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export class Product {}
